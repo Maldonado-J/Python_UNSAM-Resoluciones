e99f250b965ea4ef7ddf94f30c4edb0d168f72ef
Maldonado <maldonado.johana3@gmail.com>
2021-03-19 13:14:58 -0300

    2.8 handle missing info

--- 02_Estructuras_y_funciones/costo_camion.py
+++ 02_Estructuras_y_funciones/costo_camion.py
@@ -12,6 +12,7 @@ print('Costo total:', round(costo_total,2))
 f.close()
 
 #%% 2.6 costo_camion.py > costo_camion(nombre_archivo)
+"""
 def costo_camion(nombre_archivo):
     with open(nombre_archivo, 'rt') as f:
         headers = next(f)
@@ -24,3 +25,24 @@ def costo_camion(nombre_archivo):
 
 costo = costo_camion('../Data/camion.csv') #47671.15
 print(f'Costo total: {costo:.2f}')
+"""
+#%% 2.8 missing costo_camion(nombre_archivo)
+def costo_camion(nombre_archivo):
+    sin_precio = []
+    con_precio = []
+    with open(nombre_archivo, 'rt') as f:
+        headers = next(f)
+        costo_total = 0
+        for line in f:
+            row = line.strip('\n').split(',')
+            try:
+                costo = float(row[1]) * float(row [2])
+                costo_total += costo
+                con_precio.append(row[0])
+            except ValueError:
+                sin_precio.append(row[0])
+        return costo_total, con_precio, sin_precio
+
+costo, con_precio, sin_precio = costo_camion('../Data/missing.csv') #47671.15
+print(f'Costo total de {con_precio}: {costo:.2f}\nFaltan los precios de {sin_precio}')
+# %%
