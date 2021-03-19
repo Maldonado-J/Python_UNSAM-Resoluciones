f8b11ccab766fe69b54be1b4662de2e567d6b112
Maldonado <maldonado.johana3@gmail.com>
2021-03-19 14:25:20 -0300

    informe diccionario

--- 02_Estructuras_y_funciones/informe.py
+++ 02_Estructuras_y_funciones/informe.py
@@ -1,4 +1,5 @@
 #%% 2.15 partiendo de 2.6
+"""
 import csv
 
 def leer_camion(nombre_archivo):
@@ -9,6 +10,23 @@ def leer_camion(nombre_archivo):
         for row in lines:
             precio = round(float(row[2]),2)
             datos = (row[0], int(row[1]), precio)
+            camion.append(datos)
+        return camion
+
+camion = leer_camion('../Data/camion.csv')
+"""
+#%% 2.16 diccionario
+import csv
+
+def leer_camion(nombre_archivo):
+    camion = []
+    with open(nombre_archivo, 'rt') as f:
+        lines = csv.reader(f)
+        header = next(lines)
+        for row in lines:
+            datos = {}
+            for i in range(len(header)):
+                datos[header[i]] = row[i]
             camion.append(datos)
         return camion
 
