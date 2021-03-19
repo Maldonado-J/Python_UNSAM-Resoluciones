58ed982ad1f0efe7180da8acf16ec39647ce03de
Maldonado <maldonado.johana3@gmail.com>
2021-03-19 14:44:00 -0300

    2.17 leer_precios

--- 02_Estructuras_y_funciones/informe.py
+++ 02_Estructuras_y_funciones/informe.py
@@ -11,7 +11,7 @@ def leer_camion(nombre_archivo):
             precio = round(float(row[2]),2)
             datos = (row[0], int(row[1]), precio)
             camion.append(datos)
-        return camion
+    return camion
 
 camion = leer_camion('../Data/camion.csv')
 """
@@ -28,7 +28,23 @@ def leer_camion(nombre_archivo):
             for i in range(len(header)):
                 datos[header[i]] = row[i]
             camion.append(datos)
-        return camion
+    return camion
 
 camion = leer_camion('../Data/camion.csv')
+# %% 2.17 partiendo de 2.7
+import csv
+
+def leer_precios(nombre_archivo):
+    precios = {}
+    with open(nombre_archivo, 'rt', encoding='UTF-8') as f:
+        lines = csv.reader(f)
+        next(lines)
+        for row in lines:
+            try:
+                precios[row[0]] = round(float(row[1]), 2)
+            except:
+                pass
+    return precios
+
+precios = leer_precios('../Data/precios.csv')
 # %%
