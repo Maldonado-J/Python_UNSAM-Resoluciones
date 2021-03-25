db244a26f43430eb099ff0589037439243cb7c8c
Maldonado <maldonado.johana3@gmail.com>
2021-03-25 19:37:25 -0300

    3.3 error tipos

--- 03_Datos/solucion_de_errores.py
+++ 03_Datos/solucion_de_errores.py
@@ -40,17 +40,44 @@ def tiene_a(expresion):
 print(tiene_a('UNSAM 2020'))
 print(tiene_a('La novela 1984 de George Orwell'))
 #%% 3.3 Tipos
-""" codigo con error
-"""
 
-#Solucion
+#Error:                                        #Solucion:
+# no puedo aplicar un len() a un int    ->       si es int, tranformarlo a str
+""" codigo con error
+def tiene_uno(expresion):
+    n = len(expresion)
+    i = 0
+    tiene = False
+    while (i<n) and not tiene:
+        if expresion[i] == '1':
+            tiene = True
+        i += 1
+    return tiene
+"""
+def tiene_uno(expresion):
+    if type(expresion) == int:
+        expresion = str(expresion)
+    n = len(expresion)
+    i = 0
+    tiene = False
+    while (i<n) and not tiene:
+        if expresion[i] == '1':
+            tiene = True
+        i += 1
+    return tiene
+
+print(tiene_uno('UNSAM 2020'))
+print(tiene_uno('La novela 1984 de George Orwell'))
+print(tiene_uno(1984))
+
 #%% 3.4 Alcances
+
+#Error:                              #Solucion: 
 """ codigo con error
 """
 
-#Solucion
 #%% 3.5 Pisando memoria
+
+#Error:                              #Solucion: 
 """ codigo con error
 """
-
-#Solucion
